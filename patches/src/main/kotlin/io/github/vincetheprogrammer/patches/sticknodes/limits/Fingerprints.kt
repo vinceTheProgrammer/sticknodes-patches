36fe92b83e22ee43754d2612d506b50bc5834183
vincetheprogrammer <vincetheprogrammer@gmail.com>
2025-06-04 01:56:53 -0500

    feat: finish remove creation mode node limit patch

--- patches/src/main/kotlin/io/github/vincetheprogrammer/patches/sticknodes/limits/Fingerprints.kt
+++ patches/src/main/kotlin/io/github/vincetheprogrammer/patches/sticknodes/limits/Fingerprints.kt
@@ -16,3 +16,68 @@ val touchDownFingerprint = fingerprint {
     parameters("Lcom/badlogic/gdx/scenes/scene2d/InputEvent;", "F", "F", "I", "I")
     custom { method, classDef -> classDef.type == "Lorg/fortheloss/sticknodes/animationscreen/modules/CanvasModule$2;" }
 }
+
+val setNodeCountToolTableFingerprint = fingerprint {
+    accessFlags(AccessFlags.PUBLIC)
+    returns("V")
+    parameters("I")
+    opcodes(
+        Opcode.CONST_16,
+        Opcode.IF_LE,
+        Opcode.IPUT
+    )
+    custom { method, classDef -> classDef.type == "Lorg/fortheloss/sticknodes/animationscreen/modules/tooltables/PermanentCreationToolTable;" && method.name == "setNodeCount" }
+}
+
+val setInfoLabelToolTableFingerprint = fingerprint {
+    accessFlags(AccessFlags.PRIVATE)
+    returns("V")
+    parameters()
+    opcodes(
+        Opcode.INVOKE_STATIC,
+        Opcode.MOVE_RESULT_OBJECT,
+        Opcode.INVOKE_VIRTUAL
+    )
+    custom { method, classDef -> classDef.type == "Lorg/fortheloss/sticknodes/animationscreen/modules/tooltables/PermanentCreationToolTable;" && method.name == "setInfoLabel" }
+}
+
+val setPasteSegment1Fingerprint = fingerprint {
+    accessFlags(AccessFlags.PUBLIC)
+    returns("V")
+    parameters("Lorg/fortheloss/sticknodes/stickfigure/StickNode;", "Lorg/fortheloss/sticknodes/stickfigure/StickNode;", "Z", "Ljava/util/ArrayList;", "Lorg/fortheloss/sticknodes/stickfigure/StickNode;")
+    opcodes(
+        Opcode.IF_GT
+    )
+    custom { method, classDef -> classDef.type == "Lorg/fortheloss/sticknodes/animationscreen/modules/CanvasModule;" && method.name == "pasteSegment" }
+}
+
+val setPasteSegment2Fingerprint = fingerprint {
+    accessFlags(AccessFlags.PUBLIC)
+    returns("V")
+    parameters("Lorg/fortheloss/sticknodes/stickfigure/StickNode;", "Lorg/fortheloss/sticknodes/stickfigure/StickNode;", "Z", "Ljava/util/ArrayList;", "Lorg/fortheloss/sticknodes/stickfigure/StickNode;", "Ljava/util/ArrayList;", "Ljava/util/ArrayList;")
+    opcodes(
+        Opcode.IF_GT
+    )
+    custom { method, classDef -> classDef.type == "Lorg/fortheloss/sticknodes/animationscreen/modules/CanvasModule;" && method.name == "pasteSegment" }
+}
+
+val splitSegmentFingerprint1 = fingerprint {
+    accessFlags(AccessFlags.PUBLIC)
+    returns("V")
+    parameters("Lorg/fortheloss/sticknodes/stickfigure/StickNode;", "I")
+    custom { method, classDef -> classDef.type == "Lorg/fortheloss/sticknodes/animationscreen/modules/CanvasModule;" && method.name == "splitSegment" }
+    opcodes(
+        Opcode.IF_LE
+    )
+}
+
+val splitSegmentFingerprint2 = fingerprint {
+    accessFlags(AccessFlags.PUBLIC)
+    returns("V")
+    parameters("Lorg/fortheloss/sticknodes/stickfigure/StickNode;", "I")
+    custom { method, classDef -> classDef.type == "Lorg/fortheloss/sticknodes/animationscreen/modules/CanvasModule;" && method.name == "splitSegment" }
+    opcodes(
+        Opcode.CONST_16,
+        Opcode.IF_GT
+    )
+}
--- patches/src/main/kotlin/io/github/vincetheprogrammer/patches/sticknodes/limits/RemoveNodeLimit.kt
+++ patches/src/main/kotlin/io/github/vincetheprogrammer/patches/sticknodes/limits/RemoveNodeLimit.kt
@@ -1,10 +1,14 @@
 package io.github.vincetheprogrammer.patches.sticknodes.limits
 
+import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.patcher.extensions.InstructionExtensions.removeInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.removeInstructions
+import app.revanced.patcher.patch.rawResourcePatch
 
 val removeNodeLimitPatch = bytecodePatch(
-    name = "Remove Node Limit",
+    name = "Remove Node Limit v1.1.0",
     description = "Removes the node limit.",
 ) {
     compatibleWith(
@@ -14,9 +18,37 @@ val removeNodeLimitPatch = bytecodePatch(
 
     execute {
 
-        val ifInstruction = touchDownFingerprint.patternMatch!!.endIndex;
+        val ifTouchDownMatchEnd = touchDownFingerprint.patternMatch!!.endIndex;
+        touchDownFingerprint.method.removeInstruction(ifTouchDownMatchEnd);
 
-        touchDownFingerprint.method.removeInstruction(ifInstruction);
+        val ifToolTableMatch = setNodeCountToolTableFingerprint.patternMatch!!;
+        val ifToolTableMatchStart = ifToolTableMatch.startIndex;
+        val ifToolTableMatchEnd = ifToolTableMatch.endIndex;
+        val countToolTable = (ifToolTableMatchEnd - ifToolTableMatchStart) + 1;
+        setNodeCountToolTableFingerprint.method.removeInstructions(ifToolTableMatchStart, countToolTable)
+
+        val setInfoLabelMatchStart = setInfoLabelToolTableFingerprint.patternMatch!!.startIndex;
+        setInfoLabelToolTableFingerprint.method.addInstructions(setInfoLabelMatchStart + 2, """
+            const-string v2, "/"
+            invoke-virtual {v1, v2}, Ljava/lang/String;->indexOf(Ljava/lang/String;)I
+            move-result v3
+
+            const/4 v2, 0x0
+            invoke-virtual {v1, v2, v3}, Ljava/lang/String;->substring(II)Ljava/lang/String;
+            move-result-object v1
+            """)
+
+        val paste1MatchStart = setPasteSegment1Fingerprint.patternMatch!!.startIndex;
+        setPasteSegment1Fingerprint.method.removeInstruction(paste1MatchStart);
+
+        val paste2MatchStart = setPasteSegment2Fingerprint.patternMatch!!.startIndex;
+        setPasteSegment2Fingerprint.method.removeInstruction(paste2MatchStart);
+
+        val split1MatchStart = splitSegmentFingerprint1.patternMatch!!.startIndex;
+        splitSegmentFingerprint1.method.addInstruction(split1MatchStart, "const/4 v1, 0x0");
+
+        val split2MatchEnd = splitSegmentFingerprint2.patternMatch!!.endIndex + 1;
+        splitSegmentFingerprint2.method.removeInstruction(split2MatchEnd);
 
     }
 }
